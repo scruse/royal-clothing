46555014ad8cbfcdf56629c1e38df50f77037396
Shawn Cruse <scruse24@gmail.com>
2022-04-17 12:04:38 -0400

    created signup, form input, button comp

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+
+import './sign-up-form.styles.scss';
+
+import Button from "../ui/button.component"
+
+const defaultFormfields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormfields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormfields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      console.log("passwords do not match");
+      return;
+    }
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName: displayName });
+      resetFormFields();
+    } catch (err) {
+      if (err.code === "auth/email-already-in-use") {
+        alert("Email already in use");
+      } else {
+        console.log("user creation encountered an error", err.message);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+        <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          name="displayName"
+          onChange={handleChange}
+          value={displayName}
+          required
+        />
+
+        <FormInput
+          label="Email"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+          required
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+          required
+        />
+
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+          required
+        />
+        <Button buttonType="default" type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,64 +1,86 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAx564ajEC-1K7sMqfPhCYwCuH494ZA-TA",
-    authDomain: "royal-clothing-db-7b769.firebaseapp.com",
-    projectId: "royal-clothing-db-7b769",
-    storageBucket: "royal-clothing-db-7b769.appspot.com",
-    messagingSenderId: "40837772175",
-    appId: "1:40837772175:web:beaea0fc979da6092d51fa"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAx564ajEC-1K7sMqfPhCYwCuH494ZA-TA",
+  authDomain: "royal-clothing-db-7b769.firebaseapp.com",
+  projectId: "royal-clothing-db-7b769",
+  storageBucket: "royal-clothing-db-7b769.appspot.com",
+  messagingSenderId: "40837772175",
+  appId: "1:40837772175:web:beaea0fc979da6092d51fa",
+};
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-      prompt: "select_account"
-  });
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);  
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
 
-    console.log(userDocRef)
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot.exists())
+  console.log(userDocRef);
 
-    //if user data exists
-    if(!userSnapshot.exists()){
-        const {displayName, email} = userAuth;
-        const createdAt = new Date();
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
 
-        try{
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt,
-                // cart: {
-                //     items: []
-                // }
-            })
-        }
-         catch(err){
-             console.log('error creating user', err.message);
-         }
+  //if user data exists
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+        // cart: {
+        //     items: []
+        // }
+      });
+    } catch (err) {
+      console.log("error creating user", err.message);
     }
+  }
 
-    //if user data != exists
+  //if user data != exists
 
-    //return user date
-    return userDocRef;
-}
+  //return user date
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  try {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.log("error creating user", err.message);
+  }
+};
