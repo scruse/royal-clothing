1e369d88382170ffc45bdfbba742fa6df0d5f606
Shawn Cruse <scruse24@gmail.com>
2022-05-02 21:47:25 -0400

    styled components

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,27 @@
+import {createContext, useState, useEffect} from 'react';
+
+
+import {getCategoriesAndDocuments} from '../utils/firebase/firebase.utils';
+
+// // as the actual value you want to access
+export const CategoriesContext = createContext({
+    products: [],
+ 
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            setCategoriesMap(categoryMap);
+        }
+        getCategoriesMap();
+    }, []);
+
+    const value = {categoriesMap};
+
+
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
